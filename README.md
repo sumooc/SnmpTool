a6d65987f53af55989c6c78f82de25573f51b8a3
sumooc <pj1993@163.com>
2023-04-09 22:55:47 +0800

    Revert "修改UI方案"
    This reverts commit d3cf3d3c6ed777d2ded7d2031e677390dbd47e8a.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # SnmpTool
-
 一个SNMP连接测试工具，可以根据OID查询该OID对应的值
 工具支持
 认证协议：AES128，AES192，AES256，DES，3DES
---
+++ src/main/java/org/sumooc/SumoocAPP.java
@@ -0,0 +1,12 @@
+package org.sumooc;
+
+/**
+ * Hello world!
+ */
+public class SumoocAPP {
+
+    public static void main(String[] args) {
+        MainUI mainUI = new MainUI();
+        mainUI.ToolGUI();
+    }
+}
